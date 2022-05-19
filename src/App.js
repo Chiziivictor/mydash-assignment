06242adfa580dec05ab0a312a0a4281645d20b58
chiziivictor <chizivwokoma@gmail.com>
2022-05-19 15:53:09 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  return <main></main>;
+}
+
+export default App;
